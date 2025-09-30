5d637ee868c6661a7e812b57b951a6b40696ad6f
goldz9999 <adrianmiranda047@gmail.com>
2025-09-30 16:36:26 -0500

    codigo limpio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage';
-import TrainingPage from './Components/TrainingPage/TrainingPage';
-import EntrenarVocales from './Components/Entrenar/EntrenarVocales/EntrenarVocales';
+
+
 
 function App() {
   return (
@@ -10,8 +10,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/training" element={<TrainingPage />} />
-          <Route path="/EntrenarVocales" element={<EntrenarVocales/>} />
         </Routes>
       </div>
     </Router>
